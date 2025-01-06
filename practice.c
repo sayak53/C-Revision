cb6b7a2efcc7684155621d2a7d97f017f6dfadff
sayak53 <sayakchakraborty928@gmail.com>
2025-01-06 19:37:48 +0530

    1d array

--- practice.c
+++ practice.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 
 int main() {
-    int arr[5] = {10, 20, 30, 40, 50}; // Declaration and initialization
+    int arr[5] = {10, 20, 30, 40, 50}; 
 
-    // Accessing elements
     for (int i = 0; i < 5; i++) {
         printf("arr[%d] = %d\n", i, arr[i]);
     }

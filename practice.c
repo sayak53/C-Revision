0ef80789ecbe14dd94cea2e902ba5ff3cff9a448
sayak53 <sayakchakraborty928@gmail.com>
2025-01-06 19:40:34 +0530

    2d array

--- practice.c
+++ practice.c
@@ -1,10 +1,26 @@
+// 1d Array
+// #include <stdio.h>
+
+// int main() {
+//     int arr[5] = {10, 20, 30, 40, 50}; 
+
+//     for (int i = 0; i < 5; i++) {
+//         printf("arr[%d] = %d\n", i, arr[i]);
+//     }
+
+//     return 0;
+// }
+
+// 2d Array
 #include <stdio.h>
 
 int main() {
-    int arr[5] = {10, 20, 30, 40, 50}; 
+    int matrix[2][3] = {{1, 2, 3}, {4, 5, 6}};
 
-    for (int i = 0; i < 5; i++) {
-        printf("arr[%d] = %d\n", i, arr[i]);
+    for (int i = 0; i < 2; i++) {
+        for (int j = 0; j < 3; j++) {
+            printf("matrix[%d][%d] = %d\n", i, j, matrix[i][j]);
+        }
     }
 
     return 0;
